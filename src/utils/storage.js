0d79d3f50516fd6d8274705dde634c45bdbfe095
Wrooooooooong <mr.chen.nikoniko@gmail.com>
2021-07-14 16:38:25 +0800

    dist目录提交

--- src/utils/storage.js
+++
@@ -1,9 +0,0 @@
-const SearchHistoryKey = 'search-history'
-
-export const setSearchHistory = (searchHistoryArr) => {
-  localStorage.setItem(SearchHistoryKey, JSON.stringify(searchHistoryArr))
-}
-
-export const getSearchHistory = () => {
-  return JSON.parse(localStorage.getItem(SearchHistoryKey)) || []
-}
